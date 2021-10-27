9afbc8779fd5be3b4fadd7f876c10fdcb9d0686f
agpalberti <agonzalezp@iesrafaelalberti.es>
2021-10-27 20:56:32 +0200

    editado nombre variables

--- src/main/kotlin/ejercicio3_3_4.kt
+++ src/main/kotlin/ejercicio3_3_4.kt
@@ -4,9 +4,9 @@ class Coche1(val marca:String){
     constructor(marca:String, modelo:String, velocidadMax:Int, color:String):this(marca){
         require(velocidadMax in 11..249){"Esa velocidad no está permitida"}
 
-        val modelo = modelo
-        val velocidadMax = velocidadMax
-        val color = color
+        val modelo1 = modelo
+        val velocidadMax1 = velocidadMax
+        val color1 = color
 
     }
 }
